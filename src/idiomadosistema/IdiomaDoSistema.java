2ee875a6e75539948a557067fe95e1a6aadf7e51
Mateus Fagundes <57625884+mateusfagundes@users.noreply.github.com>
2020-04-09 14:04:48 -0300

    Inserção de arquivos
    Para a fácil execução em qualquer IDE que suporte Java

---
+++ src/idiomadosistema/IdiomaDoSistema.java
@@ -0,0 +1,10 @@
+package idiomadosistema;
+
+public class IdiomaDoSistema {
+
+    public static void main(String[] args) {
+        System.out.println("O Idioma do sistema é:");
+        System.out.println(System.getProperty("user.language"));
+    }
+    
+}
